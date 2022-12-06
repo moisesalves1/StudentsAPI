2aa367c74f808795d768203015b8aec9b5082200
Moises.alves <Moises.alves@atp.com.br>
2022-12-06 15:37:32 -0300

    API com DataAccess

---
+++ StudentServices/Controllers/StudentsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentDataAccess;
+
+namespace StudentServices.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using (StudentDetailsEntities entities = new StudentDetailsEntities())
+            {
+                return entities.Student.ToList();
+            }
+        }
+
+        public Student Get(int id)
+        {
+            using(StudentDetailsEntities entities = new StudentDetailsEntities())
+            {
+                return entities.Student.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+
+    }
+}
